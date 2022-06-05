73f199631124fba782a18ad40ad35696acdbfab6
Doug Chamberlain <dougrchamberlain@gmail.com>
2022-06-05 17:41:30 -0400

    removed warnings failing auto build

--- src/Login.tsx
+++ src/Login.tsx
@@ -44,11 +44,13 @@ export default class Login extends React.Component<any, any> {
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
+        console.log(user)
         // ...
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+        console.log(errorCode,errorMessage)
         // ..
       });
   }
